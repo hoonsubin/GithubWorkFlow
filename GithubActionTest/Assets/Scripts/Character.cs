24ee9ca02173ab54182a302d11406fcfb7367057
Sungsik Bae <46157657+Labae@users.noreply.github.com>
2021-04-14 23:05:46 +0900

    Add new project

---
+++ GithubActionTest/Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public uint HP = 10;
+
+    public void OnDamage(uint damage)
+    {
+        HP -= damage;
+    }
+}
---
+++ GithubActionTest/Assets/Tests/CharacterTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CharacterTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CharacterDamaged()
+        {
+            GameObject characterObj = new GameObject();
+            var character = characterObj.AddComponent<Character>();
+
+            character.OnDamage(5);
+
+            Assert.AreEqual(character.HP, 5);
+        }
+    }
+}
